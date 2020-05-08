66527b9ada50bf0fe1781a45147042c5aa5e4ff3
Jav-lon <javlonbek0591@gmail.com>
2020-05-08 18:20:54 +0500

    added migration init and configure db

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost:3310;dbname=demo_db',
     'username' => 'root',
-    'password' => '',
+    'password' => 'Data1234',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
